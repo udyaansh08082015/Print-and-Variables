4fcd9ade815cf0db33ea43a040c109f086861f9f
udyaansh08082015 <shubh85@gmail.com>
2026-07-02 18:28:59 +0530

    print different statements

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print ("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+# next line after print
+print("Hello Python \n")
+
+#pass multiple arguements for printing
+print("Hello",5)
+
+#end arguement of print statement
+print("welcome to",end= "*" )
